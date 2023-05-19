b8ab76951ed0ade72d2486eb81e7cfa71050f01a
EthanAmato <ethannamato@gmail.com>
2023-05-19 11:37:21 -0400

    added comment skeleton that walks student through assignment

--- src/GradeCalculator.java
+++ src/GradeCalculator.java
@@ -1,54 +1,41 @@
 import java.util.Scanner;
 
 public class GradeCalculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		// 1. Initialize any variables and objects you need (i.e. a scanner, some
+		// counters for a total running sum, and a number tracking the # of scores inputted
 
-        // Prompt for the first assignment score
-        System.out.println("Enter the assignment scores (enter a negative value to end):");
+		
+		
+		
+		
+		// 2. Make a loop that repeatedly asks the user for a number and won't stop
+		// until the user answers using a negative number
+		// 3. Add some error checking for when a user inputs a number over 100 - we
+		// should skip that input and start the loop
+		// over to ask again
 
-        // Variables to hold the sum and count of valid scores
-        int scoreSum = 0;
-        int numScores = 0;
+		
+		
+		
+		
+		// 4. Calculate the average of all scores collected during the loop
 
-        // Loop endlessly to collect assignment scores
-        while (true) {
-            // Prompt for an assignment score
-            System.out.print("Score: ");
-            int score = scanner.nextInt();
+		
+		
+		
+		
+		
+		
+		// 5. Display the average grade and corresponding message
+		// - If over 90: "Excellent Work!"
+		// - If over 80: "Good Job!"
+		// - If over 70: "Keep it up!"
+		// - anything else: "Let's work hard to get those grades up!"
 
-            // Check for termination condition
-            if (score < 0) {
-                break; // Exit the loop when a negative score is entered
-            }
-
-            // Validate the score
-            if (score > 100) {
-                System.out.println("Invalid score! Please enter a score between 0 and 100.");
-                continue; // Skip the current iteration and start the loop from the beginning to ask for another score
-            }
-
-            // Add the valid score to the sum and increment the count
-            scoreSum += score;
-            numScores++;
-        }
-
-        scanner.close();
-
-        // Calculate the average grade. Make sure to cast to a double to get a precise answer
-        double average = (double) scoreSum / numScores;
-
-        // Display the average grade and corresponding message
-        System.out.println("Average grade: " + average);
-
-        if (average >= 90) {
-            System.out.println("Excellent work!");
-        } else if (average >= 80) {
-            System.out.println("Good job!");
-        } else if (average >= 70) {
-            System.out.println("Keep it up!");
-        } else {
-            System.out.println("You need to work harder.");
-        }
-    }
+		
+		
+		
+		
+	}
 }
